2849b3e3a88e483f1c8e74c63df66d5e220330db
Chris Anderson <cda84097@gmail.com>
2019-12-09 14:15:01 -0700

    Initial Commit

---
+++ JournalWithCloudKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JournalWithCloudKit
+//
+//  Created by Chris Anderson on 12/9/19.
+//  Copyright © 2019 Renaissance Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
